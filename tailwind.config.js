72dab9fa3cf728a7ea75cb9c65a09ae79d69e0a6
Carlos Bronze <170220+bronze@users.noreply.github.com>
2021-06-05 18:38:38 -0300

    tailwind stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,29 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: {
     mode: "all",
+    content: [
+      './**/*.html',
+      './src/**/*.js',
+      './src/**/*.njk',
+      './src/**/*.md',
+    ],
     content: ["./**/*.html"],
     options: {
       whitelist: [],
     },
   },
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      indigo: colors.indigo,
+      red: colors.red,
+    },
     container: {
       center: true,
     },
